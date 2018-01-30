df6301d88357be955b866024dcba57d446f8b9bb
vilbertas <svilbertas@gmail.com>
2018-01-30 18:13:41 +0200

    exception improved

--- WpfApp1/Arithmetics.cs
+++ WpfApp1/Arithmetics.cs
@@ -23,7 +23,14 @@ namespace WpfApp1
                     result = num1 * num2;
                     break;
                 case "/":
-                    result = num1 / num2;
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        throw new DivideByZeroException();
+                    }
                     break;
             }
             return result;
